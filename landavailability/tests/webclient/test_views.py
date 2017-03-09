e432fb6ca9b3d80a1c531d0610f5228256570214
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2017-03-09 11:02:38 +0000

    Add test case to check when user is logged in

--- landavailability/tests/webclient/test_views.py
+++ landavailability/tests/webclient/test_views.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 from django.test import override_settings
 from django.test import Client
 from django.urls import reverse
+from django.contrib.auth.models import User
+import pytest
 
 
 class WebclientViewTestCase(TestCase):
@@ -11,3 +13,16 @@ class WebclientViewTestCase(TestCase):
         response = client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTrue('Sign in' in str(response.content))
+
+    @pytest.mark.django_db
+    @override_settings(STATICFILES_STORAGE=None)
+    def test_index_has_signout_when_logged(self):
+        client = Client()
+        User.objects.create_user(
+            username='testuser',
+            email='test@test.com',
+            password='testpassword')
+        client.login(username='testuser', password='testpassword')
+        response = client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('Sign out' in str(response.content))
